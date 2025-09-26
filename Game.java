9969d4e67e9e928d5e70ba28e63e7e3ee972b7ae
liuemily19 <168212021+liuemily19@users.noreply.github.com>
2025-09-25 17:04:12 -0700

    Add .java file via upload from IntelliJ
    This file includes the Game abstract class. Main variables are defined, and the Game constructor is created. The abstract methods are finished, but some of the non-abstract methods have not yet been completed.

---
+++ Game.java
@@ -0,0 +1,34 @@
+public abstract class Game {
+    private String gameName;
+    private int gameID;
+    private double requiredBet;
+    private boolean gameStarted;
+
+    public Game (String gameName, double requiredBet) {
+        this.gameName = gameName;
+        this.requiredBet = requiredBet;
+        this.gameStarted = false;
+    }
+
+    public abstract String getName();
+
+    public abstract double getRequiredBet();
+
+    public int getGameID() {
+        return gameID;
+    }
+    public void setGameID() {
+
+    }
+
+    public boolean gameStarted() {
+        return gameStarted;
+    }
+    public void setGameStarted(boolean gameStarted) {
+        this.gameStarted = gameStarted;
+    }
+
+    public String toString() {
+        return "";
+    }
+}
