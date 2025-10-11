f0fb69208318fee1847c52325d47b25ae8b0e899
gsactown30 <gsactown30@gmail.com>
2025-10-11 01:19:09 -0700

    Update Table.java
    finished playGame method to run the games repeatedly

--- Table.java
+++ Table.java
@@ -1,20 +1,47 @@
 import java.util.ArrayList;
 
 public class Table{
-    private String gameType;
+    private Game gameType;
     private int numOfPlayers;
 
-    public Table (String gameType, int numOfPlayers){
-        this.gameType = gameType;
+    public Table (String gameName, int numOfPlayers){
+        this.gameType = new gameName();
         this.numOfPlayers = numOfPlayers;
     }
 
-    public void playGame(){
-        gameType newGame = gameType();
-        newGame.play();
+    public void playGame() throws NumberOutOfRangeException{
+        //gameType newGame = gameType();
+        boolean playAgain = true;
+        gameType.play();
+        while(playAgain == true){
+            System.out.println("Would you like to play again? (Type the option number)"); // Player can choose one option to bet on
+            System.out.println("1. Yes");
+            System.out.println("2. No");
+
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+
+            if(choice != 1 && choice != 2){
+                throw new NumberOutOfRangeException("Choice must be either 1 or 2");
+            }
+            if(choice == 1){
+                continue;
+            }
+            else{
+                playAgain = false;
+            }
+        }
     }
 
     public void changeGame(){
-        
+
+    }
+
+    public String getName(){
+        return gameType.getName();
+    }
+
+    public String toString() {
+        return gameType.toString();
     }
 }
