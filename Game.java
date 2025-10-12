b749177a94759554c05da4b5c154c18df5a87a7c
liuemily19 <168212021+liuemily19@users.noreply.github.com>
2025-10-11 23:05:53 -0700

    Update Game.java
    Changed game ID setter to be assigned sequential numbers. Did this by removing the setter method and assigning game IDs directly in the constructor. Changed because it didn't make sense to have game ID randomly generated since some players would have the same game ID if they were in the same game session.

--- Game.java
+++ Game.java
@@ -1,19 +1,13 @@
-import java.util.Random;
-import java.util.List;
-import java.util.ArrayList;
-
 public abstract class Game implements Playable{
+    private static int nextGameID = 1;
     private String gameName;
     private int gameID;
     private double requiredBet;
     private boolean gameStarted;
-    private boolean isAssigned = false;
-
-    private static final List<Integer> usedGameIDs = new ArrayList<>();
-    private static int counter = 0;
 
     public Game (String gameName, double requiredBet) {
         this.gameName = gameName;
+        this.gameID = nextGameID++;
         this.requiredBet = requiredBet;
         this.gameStarted = false;
     }
@@ -24,28 +18,6 @@ public abstract class Game implements Playable{
 
     public int getGameID() {
         return gameID;
-    }
-    public void setGameID() {
-        Random rand = new Random();
-        int ID = 0;
-        boolean uniqueGameID = false;
-        
-        if (isAssigned) {
-            throw new UnsupportedOperationException("Game ID is set. No further changes allowed");
-        }
-        while (!uniqueGameID) {
-            ID = 10000 + rand.nextInt(90000);
-            uniqueGameID = true;
-            for (int i = 0; i < counter; i++) {
-                if (usedGameIDs.get(i).equals(ID)) {
-                    uniqueGameID = false;
-                    break;
-                }
-            }
-        }
-        usedGameIDs.add(ID);
-        counter++;
-        this.gameID = ID;
     }
 
     public boolean gameStarted() {
